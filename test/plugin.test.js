b76a6d78f46a2e5ff132167d3a9aca2761e4ca2e
Devin Alexander Torres <d@devinus.io>
2018-05-07 10:41:55 -0500

    Use cache-and-update strategy

--- test/plugin.test.js
+++ test/plugin.test.js
@@ -1,22 +1,26 @@
 const cheerio = require('cheerio');
 
 const serviceWorker = require('../');
-const { name: packageName } = require('../package.json');
+const { name: packageName } = require('../package');
 
 describe(packageName, () => {
   const plugin = serviceWorker();
   let files;
 
-  beforeEach(() => {
+  beforeEach((done) => {
     files = {
-      'robots.txt': { contents: new Buffer('User-agent: *\nDisallow:') },
-      'index.html': { contents: new Buffer('<html><head><link rel="stylesheet" href="http://example.com/style.css"><script src="http://example.com/script.js"></script></head><body></body></html>') },
+      'robots.txt': { contents: Buffer.from('User-agent: *\nDisallow:') },
+      'index.html': { contents: Buffer.from('<html><head><link rel="stylesheet" href="http://example.com/style.css"><script src="http://example.com/script.js"></script></head><body></body></html>') },
     };
-    plugin(files);
+    plugin(files, {}, done);
   });
 
   it('adds service worker file', () => {
     expect(files['service-worker.js']).toBeDefined();
+  });
+
+  it('adds service worker registration file', () => {
+    expect(files['service-worker-registration.js']).toBeDefined();
   });
 
   it('adds local files to service worker cache', () => {
